e786a3eac3108f25803d10022be855660a2014c0
mdnaimur <mdnaimurr@gmail.com>
2023-10-18 11:33:09 +0600

    State manage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Table from './Table';
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -38,12 +38,26 @@ class App extends Component {
         job: 'Marketing Dept.',
       },
 
-    ]
+    ],
+  }
 
+  removeCharacter = (index) => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  };
+
+  render() {
+
+    const { characters } = this.state
     return (
       <div className="container">
 
-        <Table characterData={characters} />
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     );
   }
--- src/Table.jsx
+++ src/Table.jsx
@@ -19,7 +19,9 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td> {row.name} </td>
                 <td> {row.job} </td>
-
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
@@ -34,11 +36,11 @@ const TableBody = (props) => {
 
 class Table extends Component {
     render() {
-        const { characterData } = this.props
+        const { characterData, removeCharacter } = this.props
         return (
             <table>
                 <TableHeader />
-                <TableBody characterData={characterData} />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
             </table>
         );
     }
