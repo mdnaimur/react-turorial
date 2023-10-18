fe027c1ada2f8c6b86b536aa285364c1f0bfc20e
mdnaimur <mdnaimurr@gmail.com>
2023-10-18 10:19:11 +0600

    react tutorial for contanet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Table from './Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+      {
+        name: 'Salman',
+        job: 'Sales',
+      },
+      {
+        name: 'Solaiman',
+        job: 'Software Department',
+      },
+      {
+        name: 'Zayed',
+        job: 'Medical Team',
+      },
+      {
+        name: 'Aliya',
+        job: 'Marketing Dept.',
+      },
+
+    ]
+
+    return (
+      <div className="container">
+
+        <Table characterData={characters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Table.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = (props) => {
+
+    //? map to retrive data
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td> {row.name} </td>
+                <td> {row.job} </td>
+
+            </tr>
+        )
+    })
+    //passing trive data 
+    return (
+        <tbody >
+            {rows}
+        </tbody>
+    )
+}
+
+
+class Table extends Component {
+    render() {
+        const { characterData } = this.props
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} />
+            </table>
+        );
+    }
+}
+
+export default Table;
